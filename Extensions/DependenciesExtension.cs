8b88969be69ee1e51a47dc12777215ad5f9f38f6
Ezequiel Mattos <ezequielsfc@hotmail.com>
2023-09-14 00:32:51 -0300

    Initial Commit

--- Extensions/DependenciesExtension.cs
+++ Extensions/DependenciesExtension.cs
@@ -9,10 +9,10 @@ namespace DependencyRoomBooking.Extensions;
 public static class DependenciesExtension
 {
     //exemplo
-    //public static void AddSqlConnections(this IServiceCollection services, string connectionString)
-    //{
-    //    services.AddScoped(x => new SqlConnection(connectionString));
-    //}
+    public static void AddSqlConnections(this IServiceCollection services, string connectionString)
+    {
+        services.AddScoped(x => new SqlConnection(connectionString));
+    }
 
     public static void AddRepositories(this IServiceCollection services)
     {
--- Program.cs
+++ Program.cs
@@ -6,12 +6,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddSqlConnections(connStr);
 builder.Services.AddRepositories();
 builder.Services.AddServices();
 
 var app = builder.Build();
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
